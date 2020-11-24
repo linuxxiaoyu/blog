96cf0dd548d157eaa5c6488b9fea6a7c392a1b0b
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-11-24 18:39:31 +0800

    Use gin

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/linuxxiaoyu/blog
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.62.0
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.7
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/linuxxiaoyu/blog/pkg/setting"
 )
 
 func main() {
 	setting.Init()
+
+	r := gin.Default()
+	r.Run(":8080")
 }
