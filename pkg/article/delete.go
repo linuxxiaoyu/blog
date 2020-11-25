4fd42d8b1a63128795ea5392650f0fefc09cb261
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-11-25 14:28:53 +0800

    Add article.Delete

---
+++ pkg/article/delete.go
@@ -0,0 +1,26 @@
+package article
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/linuxxiaoyu/blog/pkg/setting"
+)
+
+// Delete a article
+// DELETE /articles/:id
+// form: token
+func Delete(c *gin.Context) {
+	id, _ := strconv.ParseUint(c.Param("id"), 10, 32)
+	uid, _ := c.Get("uid")
+
+	db := setting.DB
+	result := db.Where("author_id = ?", uid.(uint)).Delete(&Article{}, uint(id))
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusForbidden, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
+}
