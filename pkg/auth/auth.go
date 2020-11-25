d9573426e8d889c32da1c758c97fe20dff70f3ee
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-11-25 11:26:44 +0800

    Add auth

---
+++ pkg/auth/auth.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/linuxxiaoyu/blog/pkg/setting"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type userClaims struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(uid uint, username string) (string, error) {
+	claims := userClaims{
+		uid,
+		username,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(2 * time.Hour).Unix(),
+			Issuer:    "blog",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(setting.SigningKey()))
+}
+
+func ParseToken(tokenString string) (*userClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &userClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(setting.SigningKey()), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*userClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, err
+}
